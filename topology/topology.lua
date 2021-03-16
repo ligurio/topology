aacbfb1c1ebcadcceceecdca251b5613df78341d
Sergey Bronnikov <sergeyb@tarantool.org>
2021-03-17 00:53:20 +0300

    Update topology methods documentation

--- topology/topology.lua
+++ topology/topology.lua
@@ -51,13 +51,7 @@ local function new(opts)
         return nil
     end
 
-    --[[
-    local conf_lib = nil
-    if backend_type == 'etcd' then
-        conf_lib = require('conf.driver.etcd')
-    end
-    ]]
-
+    -- TODO: switch to common API
     client = etcd_client_lib.new({
         endpoints = {backend.endpoints},
         user = backend.user,
@@ -76,25 +70,508 @@ end
 
 -- }}} Module functions
 
+-- {{{ Instance methods
+
+--- Instance methods.
+--
+-- @section Methods
+
+--- Add a new server to a topology.
+--
+-- Creates a key if it does not exist. Increments a revision of
+-- the key-value store. Generates one event in the event history.
+--
+-- @param self
+--     topology instance.
+-- @string name
+--     FQDN server name to add.
+--
+--     Returns the INVALID_ARGUMENT error if the key does not
+--     exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.new_server
+local function new_server(self, name)
+    local name = rawget(self, 'name')
+    local opts = opts or {}
+    return nil
+end
+
+--- Add a new Tarantool instance to a topology.
+--
+-- Creates a key if it does not exist. Increments a revision of
+-- the key-value store. Generates one event in the event history.
+-- Default state of added instance is disabled.
+--
+-- @param self
+--     topology instance.
+-- @string name
+--     Tarantool instance name to add. Name must be unique.
+-- @table[opt]   opts
+--     instance options.
+-- @string[opt]  opts.box_cfg
+--     Instance box.cfg options.
+--     See https://www.tarantool.io/en/doc/latest/reference/reference_lua/box_cfg/
+-- @string[opt] opts.replicaset_name
+--     Replicaset name.
+-- @integer[opt] opts.weight
+--     Weight value.
+-- @string[opt] opts.advertise_uri
+--     URI that will be used by clients to connect to this instance.
+-- @string[opt]  opts.zone
+--     Availability zone.
+-- @boolean[opt]  opts.is_master
+--     True if an instance is a master in replication cluster.
+--     See https://www.tarantool.io/en/doc/latest/book/replication/repl_architecture/
+-- @boolean[opt]  opts.is_storage
+--     True if an instance is a storage.
+--     See https://www.tarantool.io/en/doc/latest/reference/reference_rock/vshard/vshard_architecture/#structure
+-- @boolean[opt]  opts.is_router
+--     True if an instance is a router.
+--     See https://www.tarantool.io/en/doc/latest/reference/reference_rock/vshard/vshard_architecture/#structure
+--
+--     Returns the INVALID_ARGUMENT error if the key does not
+--     exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.new_instance
 local function new_instance(self, opts)
     local name = rawget(self, 'name')
     local opts = opts or {}
     return nil
 end
 
-local function new_replicaset(self, opts)
+--- Add new replicaset to a topology.
+--
+-- Creates a key if it does not exist. Increments a revision of
+-- the key-value store. Generates one event in the event history.
+-- Default state of added instance is disabled.
+--
+-- @param self
+--     topology instance.
+-- @string name
+--     Name of replicaset to add. Name must be unique.
+-- @array[opt]   instances
+--     Array of instances names to include to a new replicaset.
+-- @table[opt]   opts
+--     replicaset options.
+-- @string[opt]  opts.master_mode
+--     Mode that describes how master instance should be assigned.
+--     Possible values:
+--       - multimaster - it is allowed to have several instances with master role
+--         in a replication cluster, all of them should be assigned manually.
+--       - single - it is allowed to have only a single master in replication
+--         cluster that should me assigned manually.
+--       - auto - master role will be assigned automatically. Auto mode
+--         requires specifying advisory roles (leader, follower, or candidate)
+--         in Tarantool instance options (box.cfg).
+--     See https://www.tarantool.io/en/doc/latest/reference/reference_lua/box_info/election/
+-- @string[opt] opts.replicaset_name
+--     Replicaset name.
+-- @integer[opt] opts.weight
+--     Weight value.
+-- @array[opt] opts.failover_priority
+--     Array of names specifying Tarantool instances failover priority.
+-- @string[opt] opts.advertise_uri
+--     URI that will be used by clients to connect to this instance.
+-- @string[opt]  opts.zone
+--     Availability zone.
+-- @boolean[opt]  opts.is_master
+--     True if an instance is a master in replication cluster.
+--     See https://www.tarantool.io/en/doc/latest/book/replication/repl_architecture/
+-- @boolean[opt]  opts.is_storage
+--     True if an instance is a storage.
+--     See https://www.tarantool.io/en/doc/latest/reference/reference_rock/vshard/vshard_architecture/#structure
+-- @boolean[opt]  opts.is_router
+--     True if an instance is a router.
+--     See https://www.tarantool.io/en/doc/latest/reference/reference_rock/vshard/vshard_architecture/#structure
+--
+--     Returns the INVALID_ARGUMENT error if the key does not
+--     exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.new_replicaset
+local function new_replicaset(self, name, instances, opts)
     local name = rawget(self, 'name')
     local opts = opts or {}
     return nil
 end
 
-local function delete_instance(self, opts)
+--- Delete instance from a topology.
+--
+-- Deletes an instance. Deleted instance still exists in a topology,
+-- but got a status expelled and it cannot be used.
+--
+-- @param self
+--     topology instance.
+-- @string name
+--     Name of an instance to delete.
+--
+--     Returns the INVALID_ARGUMENT error if the key does not
+--     exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.delete_instance
+local function delete_instance(self, name)
     local name = rawget(self, 'name')
     local opts = opts or {}
     return nil
 end
 
-local function delete_replicaset(self, opts)
+--- Delete replicaset from a topology.
+--
+-- Deletes a replicaset.
+--
+-- @param self
+--     topology instance.
+-- @string name
+--     Name of a replicaset to delete.
+--
+--     Returns the INVALID_ARGUMENT error if the key does not
+--     exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.delete_replicaset
+local function delete_replicaset(self, name)
+    local name = rawget(self, 'name')
+    local opts = opts or {}
+    return nil
+end
+
+--- Set parameters of existed Tarantool instance in a topology.
+--
+-- Creates a key if it does not exist. Increments a revision of
+-- the key-value store. Generates one event in the event history.
+-- Default state of added instance is disabled.
+--
+-- @param self
+--     topology instance.
+-- @string name
+--     Tarantool instance name.
+-- @table[opt]   opts
+--     @{topology.new_instance|Instance options}.
+--
+--     Returns the INVALID_ARGUMENT error if the key does not
+--     exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.set_instance_property
+local function set_instance_property(self, name, opts)
+    local name = rawget(self, 'name')
+    local opts = opts or {}
+    return nil
+end
+
+--- Switch state of Tarantool instance to a reachable.
+--
+-- Creates a key if it does not exist. Increments a revision of
+-- the key-value store. Generates one event in the event history.
+-- Default state of added instance is disabled.
+--
+-- @param self
+--     topology instance.
+-- @string name
+--     Tarantool instance name.
+--
+--     Returns the INVALID_ARGUMENT error if the key does not
+--     exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.set_instance_reachable
+local function set_instance_reachable(self, name)
+    local name = rawget(self, 'name')
+    local opts = opts or {}
+    return nil
+end
+
+--- Switch state of Tarantool instance to a unreachable.
+--
+-- Cделать инстанс недоступным для использования. Он не участвует в репликации,
+-- к нему не поступают клиентские запросы, если он был в роли router и т.д.
+--
+-- @param self
+--     topology instance.
+-- @string name
+--     Tarantool instance name.
+--
+--     Returns the INVALID_ARGUMENT error if the key does not
+--     exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.set_instance_unreachable
+local function set_instance_unreachable(self, name)
+    local name = rawget(self, 'name')
+    local opts = opts or {}
+    return nil
+end
+
+--- Set topology property.
+--
+-- TODO: Cделать инстанс недоступным для использования. Он не участвует в репликации,
+-- к нему не поступают клиентские запросы, если он был в роли router и т.д.
+--
+-- @param self
+--     topology instance.
+-- @table[opt]   opts
+--     topology options.
+-- @integer[opt]  opts.rebalancer_disbalance_threshold
+--     Maximal bucket count that can be received in parallel by single replicaset.
+--     This count must be limited, because else, when a new replicaset is
+--     added to a cluster, the rebalancer would send to it very big amount of buckets
+--     from existing replicasets - it produces heavy load on a new replicaset to apply
+--     all these buckets.
+--     See https://www.tarantool.io/en/doc/latest/reference/reference_rock/vshard/vshard_architecture/#rebalancer
+-- @integer[opt]  opts.rebalancer_max_receiving
+--     maximal bucket disbalance percents. Disbalance for each replicaset is
+--     calculated by formula:
+--     |etalon_bucket_count - real_bucket_count| / etalon_bucket_count * 100.
+--     See https://www.tarantool.io/en/doc/latest/reference/reference_rock/vshard/vshard_architecture/#rebalancer
+-- @integer[opt]  opts.bucket_count
+--     Total bucket count in a cluster. It can not be changed after bootstrap!
+--
+--     Returns the INVALID_ARGUMENT error if the key does not exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.set_topology_property
+local function set_topology_property(self, opts)
+    local name = rawget(self, 'name')
+    local opts = opts or {}
+    return nil
+end
+
+--- Get routers.
+--
+-- TODO: Cделать инстанс недоступным для использования. Он не участвует в репликации,
+-- к нему не поступают клиентские запросы, если он был в роли router и т.д.
+--
+-- @param self
+--     topology instance.
+--
+--     Returns the INVALID_ARGUMENT error if the key does not exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+--
+-- TODO: routers: (array) Таблица со структурами для каждого инстанса, каждый
+-- из которых имеет роль router. Структура должна как минимум иметь: uri, name, is_master,
+-- replicaset_name.
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.get_routers
+local function get_routers(self)
+    local name = rawget(self, 'name')
+    local opts = opts or {}
+    return nil
+end
+
+--- Get storages.
+--
+-- Get storages in topology.
+--
+-- @param self
+--     topology instance.
+--
+--     Returns the INVALID_ARGUMENT error if the key does not exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+--
+-- TODO: (array) Таблица со структурами для каждого инстанса, каждый
+-- из которых имеет роль storage. Структура должна как минимум иметь: uri, name, is_master,
+-- replicaset_name.
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.get_storages
+local function get_storages(self)
+    local name = rawget(self, 'name')
+    local opts = opts or {}
+    return nil
+end
+
+--- Get instance configuration.
+--
+-- Get instance configuration.
+--
+-- @param self
+--     topology instance.
+--
+--     Returns the INVALID_ARGUMENT error if the key does not exist.
+--
+-- @raise See 'General API notes'.
+--
+-- @return Response of the following structure:
+--
+--
+-- TODO: (array) Таблица со структурами для каждого инстанса, каждый
+-- из которых имеет роль storage. Структура должна как минимум иметь: uri, name, is_master,
+-- replicaset_name.
+-- ```
+-- {
+--     header = ResponseHeader,
+--     prev_kv = KeyValue (if prev_kv is set),
+-- }
+-- ```
+--
+-- @see ResponseHeader
+-- @see KeyValue
+--
+-- @function instance.get_instance_conf
+local function get_instance_conf(self, name)
+    local name = rawget(self, 'name')
+    local opts = opts or {}
+    return nil
+end
+
+local function new_instance_link(self, opts)
+    local name = rawget(self, 'name')
+    local opts = opts or {}
+    return nil
+end
+
+local function delete_instance_link(self, opts)
+    local name = rawget(self, 'name')
+    local opts = opts or {}
+    return nil
+end
+
+local function delete(self)
     local name = rawget(self, 'name')
     local opts = opts or {}
     return nil
@@ -102,20 +579,24 @@ end
 
 mt = {
     __index = {
+        new_server = new_server,
         new_instance = new_instance,
+        new_instance_link = new_instance_link,
         new_replicaset = new_replicaset,
+
+        delete = delete,
         delete_instance = delete_instance,
+        delete_instance_link = delete_instance_link,
         delete_replicaset = delete_replicaset,
-        --delete = delete,
-        --set_instance_property = set_instance_property,
-        --set_instance_reachable = set_instance_reachable,
-        --set_instance_unreachable = set_instance_unreachable,
-        --set_topology_property = set_topology_property,
-        --get_routers = get_routers,
-        --get_storages = get_storages,
-        --get_instance_conf = get_instance_conf,
-        --new_instance_link = new_instance_link,
-        --delete_instance_link = delete_instance_link,
+
+        set_instance_property = set_instance_property,
+        set_instance_reachable = set_instance_reachable,
+        set_instance_unreachable = set_instance_unreachable,
+        set_topology_property = set_topology_property,
+
+        get_routers = get_routers,
+        get_storages = get_storages,
+        get_instance_conf = get_instance_conf,
     }
 }
 
