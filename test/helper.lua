9e15b57b36ce2235996b0943c7e510b48980c102
Sergey Bronnikov <sergeyb@tarantool.org>
2021-05-15 00:58:58 +0300

    move wait_master() function to helpers

--- test/helper.lua
+++ test/helper.lua
@@ -62,4 +62,39 @@ function helpers.gen_string(length)
     return string
 end
 
+-- FIXME: endless waiting
+local function wait_master(replicaset, master)
+    log.info('Waiting until slaves are connected to a master')
+    local all_is_ok
+    while true do
+        all_is_ok = true
+        for replica, proc in pairs(replicaset) do
+            if replica == master then
+                goto continue
+            end
+	    proc:connect_net_box()
+            local info = proc.net_box:eval('return box.info.replication')
+            if #info == 0 or #info[1] < 2 then
+                all_is_ok = false
+                goto continue
+            end
+            info = info[1]
+            for _, replica_info in pairs(info) do
+                local upstream = replica_info.upstream
+                if upstream and upstream.status ~= 'follow' then
+                    all_is_ok = false
+                    goto continue
+                end
+            end
+::continue::
+        end
+        if not all_is_ok then
+            fiber.sleep(0.1)
+        else
+            break
+        end
+    end
+    log.info('Slaves are connected to a master "%s"', master)
+end
+
 return helpers
--- test/integration/tarantool_integration_test.lua
+++ test/integration/tarantool_integration_test.lua
@@ -8,41 +8,6 @@ local helpers = require('test.helper')
 
 local g = t.group()
 
--- luacheck: no unused
-local function wait_master(replicaset, master)
-    log.info('Waiting until slaves are connected to a master')
-    local all_is_ok
-    while true do
-        all_is_ok = true
-        for replica, proc in pairs(replicaset) do
-            if replica == master then
-                goto continue
-            end
-	    proc:connect_net_box()
-            local info = proc.net_box:eval('return box.info.replication')
-            if #info == 0 or #info[1] < 2 then
-                all_is_ok = false
-                goto continue
-            end
-            info = info[1]
-            for _, replica_info in pairs(info) do
-                local upstream = replica_info.upstream
-                if upstream and upstream.status ~= 'follow' then
-                    all_is_ok = false
-                    goto continue
-                end
-            end
-::continue::
-        end
-        if not all_is_ok then
-            fiber.sleep(0.1)
-        else
-            break
-        end
-    end
-    log.info('Slaves are connected to a master "%s"', master)
-end
-
 local ETCD_ENDPOINT = 'http://127.0.0.1:2379'
 local topology_name = 'replication'
 
@@ -114,7 +79,7 @@ g.before_all(function()
         replica_1_a = replica_1_a,
         replica_1_b = replica_1_b,
     }
-    wait_master(replicaset, 'replica_1_a')
+    t.helpers.wait_master(replicaset, 'replica_1_a')
     ]]
 end)
 
