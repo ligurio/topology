111fefb3442dfb4cf020cd9c822dc42769203456
Sergey Bronnikov <sergeyb@tarantool.org>
2021-06-22 17:15:44 +0300

    minimize replication test config

--- test/integration/topology_replication.lua
+++ test/integration/topology_replication.lua
@@ -1,61 +1,34 @@
 local conf_lib = require('conf')
 local topology_lib = require('topology')
-local consts = require('topology.client.consts')
 
 local function create(topology_name, endpoints)
     -- Create a configuration client.
-    local conf_client = conf_lib.new({driver = 'etcd', endpoints = endpoints})
+    local conf_client = conf_lib.new({
+        driver = 'etcd',
+        endpoints = endpoints
+    })
     assert(conf_lib ~= nil)
 
     -- Create a topology.
     local t = topology_lib.new(conf_client, topology_name, true)
     assert(t ~= nil)
 
-    -- Create replicasets.
-    local replicaset_1_name = 'replicaset_1'
-    local replicaset_2_name = 'replicaset_2'
-    t:new_replicaset(replicaset_1_name, {
-        master_mode = consts.MASTER_MODE.MODE_AUTO,
-    })
-    t:new_replicaset(replicaset_2_name, {
-        master_mode = consts.MASTER_MODE.MODE_AUTO,
-    })
     -- Create instances.
+    local replicaset_name = 'Tweedledum_and_Tweedledee'
     t:new_instance('replica_1_a', {
         box_cfg = {
             listen = '127.0.0.1:3301',
         },
         advertise_uri = 'storage:storage@127.0.0.1:3301',
         is_master = true,
-        zone = 1,
-        replicaset = replicaset_1_name,
+        replicaset = replicaset_name,
     })
     t:new_instance('replica_1_b', {
         box_cfg = {
             listen = '127.0.0.1:3302',
         },
         advertise_uri = 'storage:storage@127.0.0.1:3302',
-        is_master = false,
-        zone = 1,
-        replicaset = replicaset_1_name,
-    })
-    t:new_instance('replica_2_a', {
-        box_cfg = {
-            listen = '127.0.0.1:3303',
-        },
-        advertise_uri = 'storage:storage@127.0.0.1:3303',
-        is_master = true,
-        zone = 1,
-        replicaset = replicaset_2_name,
-    })
-    t:new_instance('replica_2_b', {
-        box_cfg = {
-            listen = '127.0.0.1:3304',
-        },
-        advertise_uri = 'storage:storage@127.0.0.1:3304',
-        is_master = false,
-        zone = 1,
-        replicaset = replicaset_2_name,
+        replicaset = replicaset_name,
     })
 end
 
